e93e89add65ec36cb20e48c3e1fe8b391f3a1a9d
Antoine Blanco <antoine.blanco@etu.univ-poitiers.fr>
2022-01-10 17:04:35 +0100

    avancement sur la fonction add

--- map.c
+++ map.c
@@ -9,14 +9,22 @@ Map* init(index_t capacity)
     return m;
 }
 
-// TODO : may be latter check if it exists and change the value 
 _Bool add(Map* m, Cell new_cell)
 {
     if (m->size == m->capacity)
         return 0;
-    for (int i = m->size; i-->0;)
+    _Bool res = 1;
+    /*@
+    loop invariant 0 <= i <=  m->size ;
+    //loop invariant \forall integer j ; 0 <= j < i ==> res ==  (m->array[j].key != new_cell.key) && res == 0 ;
+    loop assigns i,res;
+    loop variant m->size-i ;
+    */
+    for (int i = 0; i < m->size && res != 0; i++)
         if(m->array[i].key == new_cell.key)
-            return 0;    
+            res =  0;   
+    if (res == 0) 
+        return 0;
     m->array[m->size] = new_cell;
     ++m->size;
     return 1;
--- map.h
+++ map.h
@@ -1,18 +1,9 @@
-
+#include "limits.h"
 #include "stdio.h"
 #include "stdlib.h"
-/*@
-    logic integer Capacity{L}(Map* s) = s->capacity;
-    logic integer Size{L}(Map* s) = s->size
-    logic value_type* Storage{L}(Map* s) = s->array;
-*/
 
-/*@
-    predicate
-        Empty{L}(Map* s) = Size(s) == 0;
-    predicate
-        Full{L}(Map* s) = Size(s) == Capacity(s);
-*/
+
+
 
 
 typedef int key_t;
@@ -36,13 +27,68 @@ struct Map
 typedef struct Map Map;
 
 /*@
-requires capacity: 0 < capacity;
-assigns s->obj, s->capacity, s->size;
-ensures valid: \valid(\return);
-ensures capacity: Capacity(\return) == capacity;
-//ensures invariant: Invariant(\return);
-ensures empty: Empty(\return);
+logic integer Capacity{L}(Map* s) = s->capacity;
+logic integer Size{L}(Map* s) = s->size;
 */
+
+/*@
+    predicate
+        Empty{L}(Map* s) = Size(s) == 0;
+    predicate
+        Full{L}(Map* s) = Size(s) == Capacity(s);
+    predicate
+        KeyEquals{L}(Cell c,key_t key) = c.key == key;
+    predicate
+        KeyExists{L}(Map* s,key_t key) = \exists integer indice ; 0 <= indice <= Size(s) && KeyEquals(s->array[indice],key);
+*/
+
+/*@
+requires  0 < capacity;
+*/
+//assigns s->obj, s->capacity, s->size;
+//ensures valid: \valid(\return);
+//ensures capacity: Capacity(\return) == capacity;
+//ensures invariant: Invariant(\return);
+// ensures empty: Empty(\return);
+
 Map* init(index_t capacity);
+
+
+/*@
+requires m->capacity >= m->size ;
+requires m->capacity <= INT_MAX;
+requires \valid(m);
+requires \valid(m->array + (0 .. m->capacity-1));
+behavior full:
+    assumes Full(m);
+    ensures m->size == \at(m->size,Pre);
+    ensures \result == 0;
+behavior exits:
+    assumes !Full(m) && KeyExists(m,new_cell.key);
+    ensures \result == 0;
+behavior ok:
+    assumes !Full(m) && !KeyExists(m,new_cell.key);
+    assigns m->size , m->array[m->size];
+    ensures \result == 1;
+complete behaviors ;
+disjoint behaviors ;
+*/
 _Bool add(Map* m, Cell new_cell);
 _Bool get(Map* m, key_t key,val_t *val);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
